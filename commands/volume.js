790db961a7114b421e908cabc034d65f7f7d61bb
bomie <bomidelrey@gmail.com>
2024-08-24 12:21:26 +0700

    volume.js

--- commands/volume.js
+++ commands/volume.js
@@ -8,7 +8,7 @@ async function volume(client, interaction) {
 
         if (!player) {
             const errorEmbed = new EmbedBuilder()
-                .setColor('#ff0000')
+                .setColor('#812f49')
                 .setTitle('Error')
                 .setDescription('❌ No active player found.');
 
